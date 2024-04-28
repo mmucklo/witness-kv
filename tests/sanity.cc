0b1643e2e8fbe37192dbac7e3c6beed63c5f36d1
vramesh7 <vishnuron7@gmail.com>
2024-04-28 01:21:49 +0000

    Added test fixture for setup and cleanup of kvs_server

--- tests/sanity.cc
+++ tests/sanity.cc
@@ -16,31 +16,71 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-TEST( Sanity, BasicSanityTest )
+struct SanityTests
+    : public ::testing::Test
 {
-  pid_t server_pid = fork();
-  ASSERT_NE( server_pid, -1 );
+    pid_t server_pid;
+    virtual void SetUp() override {
+        server_pid = fork();
+        ASSERT_NE( server_pid, -1 );
 
-  if ( server_pid == 0 ) {
-    // Child process: Run the server executable
-    char serverName[] = "kvs_server";
-    char* server_args[] = { serverName, nullptr };
-    execvp( server_args[0], server_args );
-    exit( 1 ); // Should not reach here if execvp succeeds
-  }
+        if ( server_pid == 0 ) {
+            // Child process: Run the server executable
+            char serverName[] = "build/server/kvs_server";
+            char* server_args[] = { serverName, nullptr };
+            ASSERT_NE( execvp( server_args[0], server_args ), -1 );
+        }
 
-  // Probably not needed, but wait a bit just to be safe.
-  std::this_thread::sleep_for( std::chrono::milliseconds( 200 ) );
+        // Probably not needed, but wait a bit just to be safe.
+        std::this_thread::sleep_for( std::chrono::milliseconds( 200 ) );
+    }
 
+    virtual void TearDown() override {
+        if (server_pid != 0) {
+            ASSERT_EQ(kill(server_pid, SIGINT), 0);
+            int status;
+            waitpid(server_pid, &status, 0);
+        }
+    }
+};
+
+TEST_F( SanityTests, BasicSanityTest )
+{
   auto channel = grpc::CreateChannel( "localhost:50051", grpc::InsecureChannelCredentials() );
   std::unique_ptr<KeyValueStore::Kvs::Stub> stub = KeyValueStore::Kvs::NewStub( channel );
   grpc::ClientContext context;
 
-  google::protobuf::Empty empty;
+  // 1. Test "Get" on empty kvs
+  {
+    KeyValueStore::KvsKey request;
+    request.set_key( "1" );
 
-  KeyValueStore::KvsKey request;
-  KeyValueStore::KvsValue response;
-  request.set_key( "1" );
-  grpc::Status status = stub->Get( &context, request, &response );
-  ASSERT_FALSE( status.ok() );
+    KeyValueStore::KvsValue response;
+    ASSERT_FALSE( stub->Get( &context, request, &response ).ok() );
+  }
+
+  // 2. Verify "Get" after a "Set" returns the expect value.
+  {
+    std::string k { "key0" };
+    std::string v { "value0" };
+    {
+      grpc::ClientContext context;
+      KeyValueStore::KvsSetRequest request;
+      google::protobuf::Empty empty;
+
+      request.set_key( k );
+      request.set_value( v );
+      ASSERT_TRUE( stub->Set( &context, request, &empty ).ok() );
+    }
+
+    {
+      grpc::ClientContext context;
+      KeyValueStore::KvsKey getRequest;
+      getRequest.set_key( k );
+      KeyValueStore::KvsValue response;
+      ASSERT_TRUE( stub->Get( &context, getRequest, &response ).ok() );
+
+      ASSERT_EQ( response.value(), v );
+    }
+  }
 }
