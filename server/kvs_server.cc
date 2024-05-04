52d4976b5557440ead6dc847b97fbf979afeb7f9
vramesh7 <vishnuron7@gmail.com>
2024-05-04 02:49:52 +0000

    Some cosmetic changes

--- server/kvs_server.cc
+++ server/kvs_server.cc
@@ -57,13 +57,10 @@ void RunServer( uint16_t port )
   builder.RegisterService( &service );
   // Finally assemble the server.
   std::unique_ptr<Server> server( builder.BuildAndStart() );
-  std::cout << "Server listening on " << server_address << std::endl;
 
   // Wait for the server to shutdown. Note that some other thread must be
   // responsible for shutting down the server for this call to ever return.
   server->Wait();
-
-  std::cout << "Server actually exited!\n";
 }
 
 int main( int argc, char** argv )
