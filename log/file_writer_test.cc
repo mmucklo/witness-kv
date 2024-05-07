4e842d6a8439e844815e8833fd1c0463772cd362
Matthew J. Mucklo <mmucklo@gmail.com>
2024-05-06 18:49:37 -0700

    More testing

--- log/file_writer_test.cc
+++ log/file_writer_test.cc
@@ -31,34 +31,40 @@ std::string getTempFilename() {
     return filename;
 }
 
+absl::Cord getLargeCord(int rounds = 128) {
+    absl::Cord cord;
+    absl::BitGen gen;
+    for (int i = 0 ; i < rounds ; i++) {
+        std::string tempstr;
+        uint32_t len = absl::Uniform(absl::IntervalClosed, gen, 1, 4096);
+        for (uint32_t j = 0; j < len ; j++) {
+            uint32_t c = absl::Uniform(absl::IntervalClosed, gen, 32, 126);
+            tempstr.append(1, (char) c);
+        }
+        cord.Append(tempstr);
+    }
+    cord.Append("This should be the end...");
+    return cord;
+}
+
 TEST(FileWriterTest, Smoke) {
     std::string filename = getTempFilename();
-    FileWriter file_writer(filename);
-    absl::Cord cord;
-    cord.Append("Some message!");
-    cord.Append("1234SecondMessage in cord.");
-    file_writer.Write(cord);
-    file_writer.Flush();
+    {
+        FileWriter file_writer(filename);
+        absl::Cord cord;
+        cord.Append("Some message!");
+        cord.Append("1234SecondMessage in cord.");
+        file_writer.Write(cord);
+        file_writer.Flush();
+    }
     ASSERT_TRUE(std::filesystem::remove(std::filesystem::path(filename)));    
 }
 
 TEST(FileWriterTest, Large) {
     std::string filename = getTempFilename();
-    absl::Cord cord;
     {
         FileWriter file_writer(filename);
-        absl::BitGen gen;
-        for (int i = 0 ; i < 128 ; i++) {
-            std::string tempstr;
-            uint32_t len = absl::Uniform(absl::IntervalClosed, gen, 1, 4096);
-            for (uint32_t j = 0; j < len ; j++) {
-                uint32_t c = absl::Uniform(absl::IntervalClosed, gen, 32, 126);
-                tempstr.append(1, (char) c);
-            }
-            cord.Append(tempstr);
-        }
-        cord.Append("Some message!");
-        cord.Append("1234SecondMessage in cord.asdf");
+        absl::Cord cord = getLargeCord();
         file_writer.Write(cord);
         file_writer.Flush();
         std::string str;
@@ -73,8 +79,29 @@ TEST(FileWriterTest, Large) {
     ASSERT_TRUE(std::filesystem::remove(std::filesystem::path(filename)));    
 }
 
-// TODO write large cord test
+TEST(FileWriterTest, LargMultiCord) {
+    std::string filename = getTempFilename();
+    {
+        FileWriter file_writer(filename);
+        absl::Cord cord1 = getLargeCord();
+        absl::Cord cord2 = getLargeCord(64);
+        file_writer.Write(cord1);
+        file_writer.Write(cord2);
+        file_writer.Flush();
+        std::string str;
+        {
+            std::ifstream fs(filename.c_str());
+            ASSERT_TRUE(fs.good());
+            str = std::string(std::istreambuf_iterator<char>{fs}, {});
+        }
+        absl::Cord combined_cord = cord1;
+        combined_cord.Append(cord2);
+        EXPECT_EQ(combined_cord, str);
+        EXPECT_EQ(combined_cord.size(), file_writer.bytes_written());
+    }
+    ASSERT_TRUE(std::filesystem::remove(std::filesystem::path(filename)));    
+}
+
 // TODO write large multi-cord test
-// TODO verify contents of file afterwards.
 
 // TODO microbenchmark to watch flush cycles and timing.
