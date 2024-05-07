6d33017b649bc14dfc4deacb41d4df4323cc5207
Matthew J. Mucklo <mmucklo@gmail.com>
2024-05-06 22:41:08 -0700

    getting absl flags working with gtest

--- log/file_writer_test.cc
+++ log/file_writer_test.cc
@@ -5,12 +5,16 @@
 #include <iostream>
 #include <iterator>
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 #include "absl/log/log.h"
 #include "absl/random/random.h"
 #include "absl/strings/cord.h"
 #include "absl/strings/str_join.h"
 #include "absl/time/time.h"
 #include <gtest/gtest.h>
+
+ABSL_FLAG(std::string, file_writer_test_flag, "", "This just a test flag");
 
 std::string getTempFilename()
 {
@@ -95,4 +99,8 @@ TEST( FileWriterTest, LargMultiCord )
   ASSERT_TRUE( std::filesystem::remove( std::filesystem::path( filename ) ) );
 }
 
+TEST( FlagTest, Smoke )
+{
+    LOG(INFO) << "file_writer_test_flag: " << absl::GetFlag(FLAGS_file_writer_test_flag);
+}
 // TODO microbenchmark to watch flush cycles and timing.
