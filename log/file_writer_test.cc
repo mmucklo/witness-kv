c7fb0cc09988574f227fb1cd24cd7ee736c4a12e
mmucklo <matthew@mucklo.com>
2024-05-06 19:45:21 -0700

    Formatting and beginning of LogWriter (#8)
    * file_writer first version
    
    * Update - add test for large cord writing,
    consolidate write path, fix a few bugs with
    writing
    
    * More testing
    
    * new lines at end of files
    
    * formatting changes plus start of log_writer

--- log/file_writer_test.cc
+++ log/file_writer_test.cc
@@ -1,107 +1,98 @@
 #include "file_writer.h"
 
 #include <filesystem>
-#include <iterator>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <iterator>
 
-#include <gtest/gtest.h>
 #include "absl/log/log.h"
 #include "absl/random/random.h"
 #include "absl/strings/cord.h"
 #include "absl/strings/str_join.h"
 #include "absl/time/time.h"
+#include <gtest/gtest.h>
 
-class FileWriterTest
-    : public ::testing::Test
+std::string getTempFilename()
 {
-    virtual void SetUp() override {
-        std::cout << "test SetUp called" << std::endl;
-    }
-
-    virtual void TearDown() override {
-        std::cout << "test TearDown called" << std::endl;
-    }
-};
-
-std::string getTempFilename() {
-    absl::Time now = absl::Now();
-    std::string filename = "/tmp/file_writer_test.";
-    filename.append(absl::StrCat(absl::ToUnixMicros(now)));
-    return filename;
+  absl::Time now = absl::Now();
+  std::string filename = "/tmp/file_writer_test.";
+  filename.append( absl::StrCat( absl::ToUnixMicros( now ) ) );
+  return filename;
 }
 
-absl::Cord getLargeCord(int rounds = 128) {
+absl::Cord getLargeCord( int rounds = 128 )
+{
+  absl::Cord cord;
+  absl::BitGen gen;
+  for ( int i = 0; i < rounds; i++ ) {
+    std::string tempstr;
+    uint32_t len = absl::Uniform( absl::IntervalClosed, gen, 1, 4096 );
+    for ( uint32_t j = 0; j < len; j++ ) {
+      uint32_t c = absl::Uniform( absl::IntervalClosed, gen, 32, 126 );
+      tempstr.append( 1, (char)c );
+    }
+    cord.Append( tempstr );
+  }
+  cord.Append( "This should be the end..." );
+  return cord;
+}
+
+TEST( FileWriterTest, Smoke )
+{
+  std::string filename = getTempFilename();
+  {
+    FileWriter file_writer( filename );
     absl::Cord cord;
-    absl::BitGen gen;
-    for (int i = 0 ; i < rounds ; i++) {
-        std::string tempstr;
-        uint32_t len = absl::Uniform(absl::IntervalClosed, gen, 1, 4096);
-        for (uint32_t j = 0; j < len ; j++) {
-            uint32_t c = absl::Uniform(absl::IntervalClosed, gen, 32, 126);
-            tempstr.append(1, (char) c);
-        }
-        cord.Append(tempstr);
-    }
-    cord.Append("This should be the end...");
-    return cord;
+    cord.Append( "Some message!" );
+    cord.Append( "1234SecondMessage in cord." );
+    file_writer.Write( cord );
+    file_writer.Flush();
+  }
+  ASSERT_TRUE( std::filesystem::remove( std::filesystem::path( filename ) ) );
 }
 
-TEST(FileWriterTest, Smoke) {
-    std::string filename = getTempFilename();
+TEST( FileWriterTest, Large )
+{
+  std::string filename = getTempFilename();
+  {
+    FileWriter file_writer( filename );
+    absl::Cord cord = getLargeCord();
+    file_writer.Write( cord );
+    file_writer.Flush();
+    std::string str;
     {
-        FileWriter file_writer(filename);
-        absl::Cord cord;
-        cord.Append("Some message!");
-        cord.Append("1234SecondMessage in cord.");
-        file_writer.Write(cord);
-        file_writer.Flush();
+      std::ifstream fs( filename.c_str() );
+      ASSERT_TRUE( fs.good() );
+      str = std::string( std::istreambuf_iterator<char> { fs }, {} );
     }
-    ASSERT_TRUE(std::filesystem::remove(std::filesystem::path(filename)));    
+    EXPECT_EQ( cord, str );
+    EXPECT_EQ( cord.size(), file_writer.bytes_written() );
+  }
+  ASSERT_TRUE( std::filesystem::remove( std::filesystem::path( filename ) ) );
 }
 
-TEST(FileWriterTest, Large) {
-    std::string filename = getTempFilename();
+TEST( FileWriterTest, LargMultiCord )
+{
+  std::string filename = getTempFilename();
+  {
+    FileWriter file_writer( filename );
+    absl::Cord cord1 = getLargeCord();
+    absl::Cord cord2 = getLargeCord( 64 );
+    file_writer.Write( cord1 );
+    file_writer.Write( cord2 );
+    file_writer.Flush();
+    std::string str;
     {
-        FileWriter file_writer(filename);
-        absl::Cord cord = getLargeCord();
-        file_writer.Write(cord);
-        file_writer.Flush();
-        std::string str;
-        {
-            std::ifstream fs(filename.c_str());
-            ASSERT_TRUE(fs.good());
-            str = std::string(std::istreambuf_iterator<char>{fs}, {});
-        }
-        EXPECT_EQ(cord, str);
-        EXPECT_EQ(cord.size(), file_writer.bytes_written());
+      std::ifstream fs( filename.c_str() );
+      ASSERT_TRUE( fs.good() );
+      str = std::string( std::istreambuf_iterator<char> { fs }, {} );
     }
-    ASSERT_TRUE(std::filesystem::remove(std::filesystem::path(filename)));    
+    absl::Cord combined_cord = cord1;
+    combined_cord.Append( cord2 );
+    EXPECT_EQ( combined_cord, str );
+    EXPECT_EQ( combined_cord.size(), file_writer.bytes_written() );
+  }
+  ASSERT_TRUE( std::filesystem::remove( std::filesystem::path( filename ) ) );
 }
-
-TEST(FileWriterTest, LargMultiCord) {
-    std::string filename = getTempFilename();
-    {
-        FileWriter file_writer(filename);
-        absl::Cord cord1 = getLargeCord();
-        absl::Cord cord2 = getLargeCord(64);
-        file_writer.Write(cord1);
-        file_writer.Write(cord2);
-        file_writer.Flush();
-        std::string str;
-        {
-            std::ifstream fs(filename.c_str());
-            ASSERT_TRUE(fs.good());
-            str = std::string(std::istreambuf_iterator<char>{fs}, {});
-        }
-        absl::Cord combined_cord = cord1;
-        combined_cord.Append(cord2);
-        EXPECT_EQ(combined_cord, str);
-        EXPECT_EQ(combined_cord.size(), file_writer.bytes_written());
-    }
-    ASSERT_TRUE(std::filesystem::remove(std::filesystem::path(filename)));    
-}
-
-// TODO write large multi-cord test
 
 // TODO microbenchmark to watch flush cycles and timing.
