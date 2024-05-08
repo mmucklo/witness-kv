7e6fdd795f8ea96f07272e1b0cec010a1b659fd0
rsingh16 <riteshkumarsingh16@gmail.com>
2024-05-08 01:22:11 -0700

    Update algorithm to handle non consesus cases in each pahses

--- paxos/proposer.cc
+++ paxos/proposer.cc
@@ -3,57 +3,66 @@
 
 void Proposer::Propose( const std::vector<std::unique_ptr<paxos::Acceptor::Stub>>& stubs, const std::string& value, const uint64_t index )
 {
-Start:
-  paxos::PrepareRequest request;
-  request.set_proposal_number( getNextProposalNumber() );
-  request.set_index_number( index );
+  bool bDone = false;
+  while (!bDone)
+  {
+    uint32_t prepMajorityCount = 0;
+    uint32_t acceptMajorityCount = 0;
+    paxos::PrepareRequest request;
+    std::string maxProposalValue = "";
+    while ( prepMajorityCount < m_majorityThreshold ) {
+      prepMajorityCount = 0;
+      request.set_proposal_number( getNextProposalNumber() );
+      request.set_index_number( index );
 
-  uint64_t maxProposalId = 0;
-  std::string maxProposalValue = "";
-  uint32_t majorityCount = 0;
+      uint64_t maxProposalId = 0;
 
-  for ( size_t i = 0; i < stubs.size(); i++ ) {
-    paxos::PrepareResponse response;
-    grpc::ClientContext context;
-    grpc::Status status = stubs[i]->Prepare( &context, request, &response );
-    if (!status.ok()) {
-      std::cerr << "Prepare grpc failed -- Maybe other node is down ?\n";
-      continue;
+      for ( size_t i = 0; i < stubs.size(); i++ ) {
+        paxos::PrepareResponse response;
+        grpc::ClientContext context;
+        grpc::Status status = stubs[i]->Prepare( &context, request, &response );
+        if ( !status.ok() ) {
+          std::cerr << "Prepare grpc failed -- Maybe other node is down ?\n";
+          continue;
+        }
+        if ( response.has_accepted_value() ) {
+          if ( maxProposalId < response.accepted_proposal() ) {
+            maxProposalId = response.accepted_proposal();
+            maxProposalValue = response.accepted_value();
+          }
+          ++prepMajorityCount;
+        } else {
+          maxProposalValue = value;
+          ++prepMajorityCount;
+        }
+      }
     }
 
-    if (response.has_accepted_value()) {
-      maxProposalValue = response.accepted_value();
-      ++majorityCount;
-    }
-    else {
-      maxProposalValue = value;
-      ++majorityCount;
-    }
-  }
-
-  if ( majorityCount >= m_majorityThreshold ) {
     paxos::AcceptRequest accept_request;
     accept_request.set_proposal_number( request.proposal_number() );
     accept_request.set_index_number( request.index_number() );
     accept_request.set_value( maxProposalValue );
+    paxos::AcceptResponse accept_response;
 
     for ( size_t i = 0; i < stubs.size(); i++ ) {
       grpc::ClientContext context;
-      paxos::AcceptResponse accept_response;
-      grpc::Status status = stubs[i]->Accept( &context, accept_request, &accept_response );
+      grpc::Status status
+          = stubs[i]->Accept( &context, accept_request, &accept_response );
       if ( !status.ok() ) {
         std::cerr << "Accept GRPC Failed\n";
         continue;
-      } 
-      else {
-        std::cout << "Accepted Proposal number: " << accept_response.min_proposal() << ", accepted value: " << maxProposalValue << ", at index: " << request.index_number()
-                << "\n";
+      } else {
+        if ( accept_response.min_proposal() == request.proposal_number() ) {
+          ++acceptMajorityCount;
+        }
       }
     }
-  }
-  else
-  {
-      // FIXME: modularize it later
-      goto Start;
+    if ( acceptMajorityCount >= m_majorityThreshold ) {
+      std::cout << "Accepted Proposal number: "
+                << accept_response.min_proposal()
+                << ", accepted value: " << maxProposalValue
+                << ", at index: " << request.index_number() << "\n";
+      bDone = true;
+    }
   }
 }
